a2de80f13267a9def8fd49c7c4abca7c129d683d
Santiago Otálvaro <sotalvaroo1999@gmail.com>
2024-07-04 00:54:37 -0500

    Cambiecito

--- MultiProject.SampleLib/SampleClass.cs
+++ MultiProject.SampleLib/SampleClass.cs
@@ -4,5 +4,15 @@ namespace MultiProject.SampleLib
     public class SampleClass
     {
         public int Random { get; set; }
+
+        public SampleClass(int random)
+        {
+            Random = random;
+        }
+
+        public int GetRandomNumber()
+        {
+            return Random;
+        }
     }
 }
--- MultiProject/UnitTest1.cs
+++ MultiProject/UnitTest1.cs
@@ -1,3 +1,5 @@
+using MultiProject.SampleLib;
+
 namespace MultiProject
 {
     public class UnitTest1
@@ -5,7 +7,9 @@ namespace MultiProject
         [Fact]
         public void Test1()
         {
-
+            SampleClass c = new SampleClass(1);
+            var myInt = c.Random;
+            Assert.Equal(1, myInt);
         }
     }
 }
